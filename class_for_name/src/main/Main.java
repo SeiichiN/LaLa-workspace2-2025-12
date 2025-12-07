1c06ac8e50b3c8e423af8055a85fcf074fc2e3ca
SeiichiN <billie175@gmail.com>
2025-12-07 11:28:16 +0900

    class_for_name added.

---
+++ class_for_name/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName("main.Hero");
+		// Hero h = new Hero();
+		// System.out.println(h);
+	}
+
+}
